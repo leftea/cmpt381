91b173cbaea8ebcbecd84b878e81a15f52554b9c
matthew galbraith <m.g@usask.ca>
2016-03-07 15:31:39 -0600

    Create IdeaShape Model Holds bitmap information of the visualized idea in a shape, reference to the associated IdeaCanvas

---
+++ app/src/main/java/com/example/wizard/cmpt381/IdeaShape.java
@@ -0,0 +1,53 @@
+package com.example.wizard.cmpt381;
+
+import android.graphics.Bitmap;
+
+/**
+ * IdeaShape: Data Model for the visualized ideas on MainView. Holds bitmaps and other necessary
+ * attributes
+ */
+public class IdeaShape {
+    private Bitmap mIdeaBitmap;
+    private Bitmap mShapeBitmap;
+    private Bitmap mIdeaShapeBitmap;
+    private IdeaCanvas pIdeaCanvas;
+
+    public IdeaShape(Bitmap mIdeaBitmap, Bitmap mShapeBitmap, Bitmap mIdeaShapeBitmap, IdeaCanvas pIdeaCanvas) {
+        this.mIdeaBitmap = mIdeaBitmap;
+        this.mShapeBitmap = mShapeBitmap;
+        this.mIdeaShapeBitmap = mIdeaShapeBitmap;
+        this.pIdeaCanvas = pIdeaCanvas;
+    }
+
+    public Bitmap getmIdeaBitmap() {
+        return mIdeaBitmap;
+    }
+
+    public void setmIdeaBitmap(Bitmap mIdeaBitmap) {
+        this.mIdeaBitmap = mIdeaBitmap;
+    }
+
+    public Bitmap getmShapeBitmap() {
+        return mShapeBitmap;
+    }
+
+    public void setmShapeBitmap(Bitmap mShapeBitmap) {
+        this.mShapeBitmap = mShapeBitmap;
+    }
+
+    public Bitmap getmIdeaShapeBitmap() {
+        return mIdeaShapeBitmap;
+    }
+
+    public void setmIdeaShapeBitmap(Bitmap mIdeaShapeBitmap) {
+        this.mIdeaShapeBitmap = mIdeaShapeBitmap;
+    }
+
+    public IdeaCanvas getpIdeaCanvas() {
+        return pIdeaCanvas;
+    }
+
+    public void setpIdeaCanvas(IdeaCanvas pIdeaCanvas) {
+        this.pIdeaCanvas = pIdeaCanvas;
+    }
+}
