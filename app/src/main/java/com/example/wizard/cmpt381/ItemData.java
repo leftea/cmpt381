9385bea51121d603d14681e1625008cb7e1801c6
srd807 <srd807@mail.usask.ca>
2016-04-02 19:52:26 -0600

    Removed some commented out code from custom spinner and associated classes.

--- app/src/main/java/com/example/wizard/cmpt381/ItemData.java
+++ app/src/main/java/com/example/wizard/cmpt381/ItemData.java
@@ -1,17 +1,11 @@
 package com.example.wizard.cmpt381;
 
 public class ItemData {
-    //String text;
     Integer imageId;
 
     public ItemData(Integer imageId) {
-        //this.text=text;
         this.imageId = imageId;
     }
-
-    /*public String getText(){
-        return text;
-    }*/
 
     public Integer getImageId() {
         return imageId;
--- app/src/main/java/com/example/wizard/cmpt381/SpinnerAdapter.java
+++ app/src/main/java/com/example/wizard/cmpt381/SpinnerAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,8 +28,6 @@ public class SpinnerAdapter extends ArrayAdapter<ItemData> {
         View itemView = inflater.inflate(groupId, parent, false);
         ImageView imageView = (ImageView) itemView.findViewById(R.id.img);
         imageView.setImageResource(list.get(position).getImageId());
-        //TextView textView=(TextView)itemView.findViewById(R.id.txt);
-        //textView.setText(list.get(position).getText());
         return itemView;
     }
 
