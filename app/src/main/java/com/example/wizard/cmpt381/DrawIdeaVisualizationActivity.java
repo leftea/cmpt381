7e07578841c994f58d6e15635867c2afc01136ea
srd807 <srd807@mail.usask.ca>
2016-04-04 17:46:05 -0600

    added functionality to drawIdeaVisualization for eraser, paintbrush, and color palette. changed some names, and deleted the useless + button on main activity. also deleted some useless button selected code.

--- app/src/main/java/com/example/wizard/cmpt381/DrawIdeaVisualizationActivity.java
+++ app/src/main/java/com/example/wizard/cmpt381/DrawIdeaVisualizationActivity.java
@@ -1,34 +1,73 @@
 package com.example.wizard.cmpt381;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import com.example.wizard.cmpt381.DrawingTools.EraserCreator;
+import com.example.wizard.cmpt381.DrawingTools.SimpleBrushCreator;
 
 import java.util.ArrayList;
 
 public class DrawIdeaVisualizationActivity extends AppCompatActivity {
 
+    private static final int PAINTBRUSH = 0;
+    private static final int ERASER = 1;
+
+    private PaintState fPaintState;
+    private Context fContext;
+    private ImageButton paletteBtn;
+    private int backgroundColor;
+    private int textColor;
 
     private final String TAG = "DrawIVActivity";
     private DrawManager fManager;
     private IdeaVisualizationView fDrawView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_draw_idea_visualization);
         fDrawView = (IdeaVisualizationView) findViewById(R.id.baseIdeaView);
         fManager = fDrawView.getCanvasManager();
+        fContext = this;
+        fPaintState = fManager.getPaintState();
 
-        findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
+        fManager.addTool(PAINTBRUSH, new SimpleBrushCreator(fManager, fDrawView));
+        fManager.setCurrentTool(0);
+        ((ImageButton) findViewById(R.id.imageBrushButtonV)).getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+
+        TypedArray array = getTheme().obtainStyledAttributes(new int[]{
+                android.R.attr.colorBackground,
+                android.R.attr.textColorPrimary,
+        });
+
+        backgroundColor = array.getColor(0, 0xFF00FF);
+        textColor = array.getColor(1, 0xFF00FF);
+        array.recycle();
+
+        fManager.addTool(ERASER, new EraserCreator(fManager, fDrawView, backgroundColor));
+
+        fDrawView.setClickable(false);
+
+        addListenerOnPaletteButton();
+
+        findViewById(R.id.saveButtonV).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(DrawIdeaVisualizationActivity.this, MainActivity.class));
             }
         });
+
         ArrayList<ItemData> list = new ArrayList<>();
         list.add(new ItemData(R.mipmap.ic_launcher_rectangle));
         list.add(new ItemData(R.mipmap.ic_launcher_circle));
@@ -62,18 +101,61 @@ public class DrawIdeaVisualizationActivity extends AppCompatActivity {
 
             }
         });
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //       setSupportActionBar(toolbar);
-/*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.imageBrushButtonV).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                ImageButton imageBrushBtn = (ImageButton) v;
+                imageBrushBtn.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                ImageButton eraserBtn = (ImageButton) findViewById(R.id.eraserButtonV);
+                eraserBtn.getBackground().clearColorFilter();
+                fManager.setCurrentTool(PAINTBRUSH);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+
+        findViewById(R.id.eraserButtonV).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageButton eraserBtn = (ImageButton) v;
+                eraserBtn.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                ImageButton imageBrushBtn = (ImageButton) findViewById(R.id.imageBrushButtonV);
+                imageBrushBtn.getBackground().clearColorFilter();
+                fManager.setCurrentTool(ERASER);
+            }
+        });
+
+    }
+
+    public void addListenerOnPaletteButton() {
+
+        paletteBtn = (ImageButton) findViewById(R.id.paletteButtonV);
+
+        paletteBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                showColorPickerDialog();
+            }
+
+        });
+
+    }
+
+    private void showColorPickerDialog() {
+
+        int initialColor = Color.BLACK;
+
+        ColorPickerDialog colorPickerDialog = new ColorPickerDialog(this, initialColor, new ColorPickerDialog.OnColorSelectedListener() {
+
+            public void onColorSelected(int color) {
+                fManager.getPaintState().setColor(color);
+            }
+
+        });
+
+        colorPickerDialog.show();
     }
 
 }
+
